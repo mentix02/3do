373aa2e268a5cf5c773ac42bab7b0a28dc800752
mentix02 <manan.yadav02@gmail.com>
2021-11-09 20:21:57 +0530

    Converted tabs to spaces.

--- exp/routes.js
+++ exp/routes.js
@@ -5,44 +5,44 @@ const router = express.Router();
 
 // Get all tasks
 router.get("/tasks", async (_, res) => {
-	const tasks = await Task.find();
-	res.json(tasks);
+  const tasks = await Task.find();
+  res.json(tasks);
 });
 
 // Create a new task
 router.post("/tasks", async (req, res) => {
-	const task = new Task(req.body);
-	await task.save();
-	res.status(201).json(task);
+  const task = new Task(req.body);
+  await task.save();
+  res.status(201).json(task);
 });
 
 // Get a task by id
 router.get("/tasks/:id", async (req, res) => {
-	const task = await Task.findById(req.params.id);
-	if (!task) {
-		return res.status(404).send("Task not found");
-	}
-	res.json(task);
+  const task = await Task.findById(req.params.id);
+  if (!task) {
+    return res.status(404).send("Task not found");
+  }
+  res.json(task);
 });
 
 // Delete a task by id
 router.delete("/tasks/:id", async (req, res) => {
-	const task = await Task.findByIdAndDelete(req.params.id);
-	if (!task) {
-		return res.status(404).send("Task not found");
-	}
-	res.json({ message: "Task deleted" });
+  const task = await Task.findByIdAndDelete(req.params.id);
+  if (!task) {
+    return res.status(404).send("Task not found");
+  }
+  res.json({ message: "Task deleted" });
 });
 
 // Update a task by id
 router.patch("/tasks/:id", async (req, res) => {
-	const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-		new: true,
-	});
-	if (!task) {
-		return res.status(404).send("Task not found");
-	}
-	res.json(task);
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  if (!task) {
+    return res.status(404).send("Task not found");
+  }
+  res.json(task);
 });
 
 module.exports = router;
